689cb1034fe77ace3ae8a89db81ac8c2a2ee1279
Andres rivera <andresrivera2414@gmail.com>
2025-07-01 14:10:41 -0400

    adding num 0

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -1,12 +1,13 @@
-#include "main.h"More actions
+#include "main.h"
 /**
  * reset_to_98 - this program resets the value of an int to 98
  *
  * @n: the value of deferencing pointer
  *
- * Return: nth
+ * Return: Always 0
  */
 void reset_to_98(int *n)
 {
 	*n = 98;
+Return: (0);
 }
